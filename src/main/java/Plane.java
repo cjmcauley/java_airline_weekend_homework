fd35613b713cf196ef78a140a3c2be6c5e9a64c2
cjmcauley <christopher.mcauley@gmail.com>
2019-09-06 14:08:08 +0100

    initial commit

---
+++ src/main/java/Plane.java
@@ -0,0 +1,23 @@
+public enum Plane {
+
+    AIRBUSA220(30, 350),
+    AIRBUSA330(20, 300),
+    BOEING747(40, 350),
+    BOEING737(30, 450);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    Plane(int capacity, int totalWeight){
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+}
